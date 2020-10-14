6c0a33e2aae941c540a95eb0b696d4160560b46c
unknown <camgs@hotmail.com>
2020-10-14 15:42:20 -0500

    todo proyecto

---
+++ ProyectoAyuda2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProyectoAyuda2.Global" Language="C#" %>
---
+++ ProyectoAyuda2/UnirPDF.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
+namespace ProyectoAyuda2
+{
+    public partial class UnirPDF : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnirPDFxcx();
+        }
+
+        internal static bool Merge(string strFileTarget, string[] arrStrFilesSource)
+        {
+            bool blnMerged = false;
+
+            // Crea el PDF de salida
+            try
+            {
+                using (System.IO.FileStream stmFile = new System.IO.FileStream(strFileTarget, System.IO.FileMode.Create))
+                {
+                    Document objDocument = null;
+                    PdfWriter objWriter = null;
+
+                    // Recorre los archivos
+                    for (int intIndexFile = 0; intIndexFile < arrStrFilesSource.Length; intIndexFile++)
+                    {
+                        PdfReader objReader = new PdfReader(arrStrFilesSource[intIndexFile]);
+                        int intNumberOfPages = objReader.NumberOfPages;
+
+                        // La primera vez, inicializa el documento y el escritor
+                        if (intIndexFile == 0)
+                        { // Asigna el documento y el generador
+                            objDocument = new Document(objReader.GetPageSizeWithRotation(1));
+                            objWriter = PdfWriter.GetInstance(objDocument, stmFile);
+                            // Abre el documento
+                            objDocument.Open();
+                        }
+                        // Añade las páginas
+                        for (int intPage = 0; intPage < intNumberOfPages; intPage++)
+                        {
+                            int intRotation = objReader.GetPageRotation(intPage + 1);
+                            PdfImportedPage objPage = objWriter.GetImportedPage(objReader, intPage + 1);
+
+                            // Asigna el tamaño de la página
+                            objDocument.SetPageSize(objReader.GetPageSizeWithRotation(intPage + 1));
+                            // Crea una nueva página
+                            objDocument.NewPage();
+                            // Añade la página leída
+                            if (intRotation == 90 || intRotation == 270)
+                                objWriter.DirectContent.AddTemplate(objPage, 0, -1f, 1f, 0, 0,
+                                                                    objReader.GetPageSizeWithRotation(intPage + 1).Height);
+                            else
+                                objWriter.DirectContent.AddTemplate(objPage, 1f, 0, 0, 1f, 0, 0);
+                        }
+                    }
+                    // Cierra el documento
+                    if (objDocument != null)
+                        objDocument.Close();
+                    // Cierra el stream del archivo
+                    stmFile.Close();
+                }
+                // Indica que se ha creado el documento
+                blnMerged = true;
+            }
+            catch (Exception objException)
+            {
+                System.Diagnostics.Debug.WriteLine(objException.Message);
+            }
+            // Devuelve el valor que indica si se han mezclado los archivos
+            return blnMerged;
+        }
+
+        public void UnirPDFxcx() {
+            List<byte[]> pdf = new List<byte[]>();
+
+
+            byte[] by_1 = fileToByteArray(@"D:\DescargasChrome\Constancia_virtual 20202[R].pdf");
+            byte[] by_2 = fileToByteArray(@"D:\DescargasChrome\Constancia_virtual 20203[R].pdf");
+            pdf.Add(by_1);
+            pdf.Add(by_2);
+            byte[] mergedPdf = null;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Document document = new Document())
+                {
+                    using (PdfCopy copy = new PdfCopy(document, ms))
+                    {
+                        document.Open();
+
+                        for (int i = 0; i < pdf.Count; ++i)
+                        {
+                            PdfReader reader = new PdfReader(pdf[i]);
+                            // loop over the pages in that document
+                            int n = reader.NumberOfPages;
+                            for (int page = 0; page < n;)
+                            {
+                                copy.AddPage(copy.GetImportedPage(reader, ++page));
+                            }
+                        }
+                    }
+                }
+                mergedPdf = ms.ToArray();
+            }
+
+
+            Response.Clear();
+            Response.ContentType = "application/pdf";
+            Response.AddHeader("content-disposition", string.Format("attachment;filename={0}", "Document_" + DateTime.Now.ToShortDateString().ToString() + ".pdf"));
+            Response.BinaryWrite(mergedPdf);
+            Response.End();
+
+        }
+
+        public byte[] fileToByteArray(String fileName)
+        {
+            BinaryReader binReader = new BinaryReader(File.Open(fileName, FileMode.Open, FileAccess.Read));
+            binReader.BaseStream.Position = 0;
+            byte[] binFile = binReader.ReadBytes(Convert.ToInt32(binReader.BaseStream.Length));
+            binReader.Close();
+            return binFile;
+        }
+
+    }
+}
